84c5038ea856c68452b951f47ac37c8013ae34c1
Troy Allen <jake.allen@me.com>
2022-05-18 22:02:58 -0400

    restyled

--- index.php
+++ index.php
@@ -3,15 +3,25 @@
     <div class="header">
         <div class="content">
             <div class="image">
-                <img src="assets/img/palm-tree.jpg" />
+                <img src="/assets/img/anneSitting.jpg" />
+                <h2>Anne Thomas - Owner and Creator of 'Flock' and Pottery</h2>
             </div>
             <div class="info">
                 <div class="infoBox">
                     <h1>Purple Flamingo Flock</h1>
                     <p>Local Artist Consortium</p>
+                    <p>A division of Purple Flamingo Flock</p>
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="featuredProducts">
+        <div class="content">
+            <img src="/assets/img/troyAnne.jpg">
+            <h3>Anne with Troy Allen, Creater of Skunkworks Products</h3>
+        </div>
+    </div>
     </div>
 
     <div class="featuredProducts">
@@ -36,13 +46,33 @@
 
     <div class="about" id="about">
         <div class="content">
-            <h2>About</h2>
-            <p>
-                Purple Flamingo Flock is an artist supporting organization developed by Anne Thomas’ desire to
-                support and enable artists who exhibit creativity and desire to succeed. Creators are free to
-                develop their businesses to develop their skills and niches under the tutelage of, and support by,
-                the “Flock.”
-            </p>
+            <div class="contentBlock">
+                <h2>About Purple Flamingo Flock</h2>
+                <h3>Anne B. Thomas</h3>
+                <p>
+                    Purple Flamingo Flock is an artist supporting organization developed by Anne Thomas’ desire to
+                    support and enable artists who exhibit creativity and desire to succeed. Creators are free to
+                    develop their businesses to develop their skills and niches under the tutelage of, and support by,
+                    the “Flock.”
+                </p>
+                <h3>Members of the Flock</h3>
+                <ul>
+                    <li><a href="https://www.bentlyskunkworks.com">Bently Skunk Works</a></li>
+                    <li><a href="https://www.ahartseyephotography.com">A Hart's Eye Photography</a></li>
+                    <li><a href="https://www.greygato.com">Grey Gato Media</a></li>
+                </ul>
+            </div>
+            <div class="contentBlock">
+                <h2>About Purple Flamingo Pottery</h2>
+                <h3>Troy Allen and Anne B. Thomas</h3>
+                <p>
+                    Purple Flamingo Pottery is the brainchild of Anne Thomas. When a change in direction was in the
+                    wind, she turned to pottery as an outlet to enable her creative mind to produce amazing ceramic
+                    artwork. Since forming Purple Flamingo Pottery in 2018, in Duncanville Texas, she has since
+                    outfitted another studio in Fort Myers Florida. There, Troy Allen shares the space creating his
+                    artistry under the “Bently Skunkworks” name.
+                </p>
+            </div>
         </div>
     </div>
     <?php include_once "footer.php" ?>
